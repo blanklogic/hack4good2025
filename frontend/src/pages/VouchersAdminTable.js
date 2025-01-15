a203e08798bc7e1278b516b1cfcd51931a161aed
Leow Sze Theng, Jolyn <jolynleow2004@gmail.com>
2025-01-15 23:28:10 +0800

    Add notifications

--- frontend/src/pages/VouchersAdminTable.js
+++ frontend/src/pages/VouchersAdminTable.js
@@ -1,11 +1,39 @@
-import React from "react";
+import { React, useState } from "react";
 import "../index.css";
+import { ToastContainer, toast } from "react-toastify";
 
 const VouchersAdminTable = (props) => {
   let data = props.tableData;
+  const [isVisible, setIsVisible] = useState(true);
+
   let num = 0;
+  let notif = "";
+  const success = () =>
+    toast.success(notif, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  const reject = () => toast.error(notif);
+  const handleApprove = () => {
+    notif = "Voucher Approved!";
+    success();
+    setIsVisible(false);
+  };
+  const handleReject = () => {
+    notif = "Voucher Rejected!";
+    reject();
+    setIsVisible(false);
+  };
+
   return (
     <div className="h-full w-auto overflow-y-auto">
+      <ToastContainer position="top-right" theme="light" />
       <table>
         <thead>
           <tr>
@@ -18,8 +46,12 @@ const VouchersAdminTable = (props) => {
           <td className="table-data">{num}</td>
           <td className="table-data">{"Food and Beverages"}</td>
           <td className="buttons-dist">
-            <button className="approve-button">Approve</button>
-            <button className="reject-button">Reject</button>
+            <button className="approve-button" onClick={() => handleApprove()}>
+              Approve
+            </button>
+            <button className="reject-button" onClick={() => handleReject()}>
+              Reject
+            </button>
           </td>
         </tbody>
         {/* <tbody>
