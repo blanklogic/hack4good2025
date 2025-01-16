6cd5ac344bfe002a885d70a4f402d049a72e5259
Leow Sze Theng, Jolyn <jolynleow2004@gmail.com>
2025-01-17 00:16:45 +0800

    Added UI functionality to create product

--- frontend/src/pages/Inventory.js
+++ frontend/src/pages/Inventory.js
@@ -1,11 +1,11 @@
 import { React, useState } from "react";
 import "../index.css";
 import { useNavigate } from "react-router";
+import { ToastContainer, toast } from "react-toastify";
 
 const Inventory = () => {
   const navigate = useNavigate();
-  const [currentStep, setCurrentStep] = useState(1);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [currentStep, setCurrentStep] = useState(0);
 
   // Step 1: Add title and description
   const [title, setTitle] = useState("");
@@ -18,7 +18,7 @@ const Inventory = () => {
 
   const handleContinueAfterTitleAndDescClick = () => {
     if (title === "") {
-      setErrorMessage("Please enter the title.");
+      toast.error("Please enter the title.");
       return;
     }
     if (description === "") {
@@ -27,18 +27,75 @@ const Inventory = () => {
     console.log(title, description);
 
     if (title.length > 255) {
-      setErrorMessage(
+      toast.error(
         "The name can be up to 255 characters long. Valid characters include all alphanumeric characters and spaces."
       );
       return;
     }
     if (description.split(" ").length > 120) {
-      setErrorMessage("The description exceeds the maximum word count of 120.");
+      toast.error("The description exceeds the maximum word count of 120.");
       return;
     }
-
-    setErrorMessage(""); // Clear error message
     setCurrentStep((prevStep) => prevStep + 1); // Proceed to next step
+  };
+
+  // Step 2: Add price and stock availability
+  const [price, setPrice] = useState("");
+  const [stock, setStock] = useState("");
+  const [images, setImages] = useState([]);
+
+  const handleUploadImageClick = (inputId) => {
+    /** @type {HTMLInputElement} */
+    const fileInput = document.getElementById(inputId);
+
+    if (fileInput) {
+      fileInput.click();
+    } else {
+      console.error(`File input with ID "${inputId}" not found.`);
+    }
+  };
+
+  const handleImageUpload = (e) => {
+    if (!e.target.files) return; // Early exit if no files are selected
+
+    const filesArray = Array.from(e.target.files);
+    const validFormats = ["image/jpeg", "image/jpg", "application/pdf"];
+
+    const validFiles = filesArray.filter((file) => {
+      if (validFormats.includes(file.type)) {
+        return true;
+      } else {
+        toast.error(
+          "Invalid format for pictures. Only jpeg, jpg, and pdf files are allowed."
+        );
+        return false;
+      }
+    });
+
+    setImages((prev) => [...prev, ...validFiles]);
+  };
+
+  const handleDeleteImage = () => {
+    setImages([]);
+  };
+
+  const handleContinueAfterOtherDetailsClick = () => {
+    if (!price || !stock || images.length === 0) {
+      toast.error("Please enter all fields.");
+      return;
+    } else {
+      console.log(price, stock, images);
+      setCurrentStep((prevStep) => prevStep + 1); // Proceed to next step
+    }
+  };
+
+  // Step 3: Summary and Submit
+
+  const handleSubmitButtonClick = () => {
+    console.log("Form submitted");
+    toast.success("Product added successfully!");
+    setCurrentStep(0);
+    navigate("/admin/inventory");
   };
 
   const renderStep = () => {
@@ -61,6 +118,7 @@ const Inventory = () => {
       case 1: // Title and Desc
         return (
           <div className="add-product-container">
+            <ToastContainer />
             <h2 className="text-5xl font-bold text-white mb-8">
               Add a Product
             </h2>
@@ -77,7 +135,6 @@ const Inventory = () => {
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             ></textarea>
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
             <div className="action-buttons">
               <button className="cancel-button" onClick={handleCancelClick}>
                 Cancel
@@ -91,6 +148,106 @@ const Inventory = () => {
             </div>
           </div>
         );
+      case 2: // Other details
+        return (
+          <div className="add-product-container">
+            <ToastContainer />
+            <h2 className="text-5xl font-bold text-white mb-8">
+              Other details
+            </h2>
+            <input
+              type="text"
+              placeholder="Input price"
+              className="input-title"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Input stock availability"
+              className="input-title"
+              value={stock}
+              onChange={(e) => setStock(e.target.value)}
+            />
+            <div className="upload-image-container">
+              {images.length > 0 && (
+                <div className="image-preview-wrapper">
+                  <img
+                    src={URL.createObjectURL(images[0])}
+                    alt={`preview-${images[0].name}`}
+                    className="preview-image"
+                  />
+                  <button
+                    className="delete-button"
+                    onClick={() => handleDeleteImage()}
+                  >
+                    x
+                  </button>
+                </div>
+              )}
+              {images.length === 0 && (
+                <div className="upload-section">
+                  <button
+                    className="upload-button"
+                    onClick={() => handleUploadImageClick("item-images-input")}
+                  >
+                    + Upload Image
+                  </button>
+                  <input
+                    type="file"
+                    id="item-images-input"
+                    accept=".jpg,.jpeg,.pdf"
+                    multiple
+                    onChange={handleImageUpload}
+                    className="hidden-input"
+                  />
+                </div>
+              )}
+            </div>
+            <div className="action-buttons">
+              <button className="cancel-button" onClick={handleCancelClick}>
+                Cancel
+              </button>
+              <button
+                className="continue-button"
+                onClick={handleContinueAfterOtherDetailsClick}
+              >
+                Continue
+              </button>
+            </div>
+          </div>
+        );
+      case 3: // Summary and Submit
+        return (
+          <div className="add-product-container">
+            <ToastContainer />
+            <h2 className="text-5xl font-bold text-white mb-8">Summary</h2>
+            <div className="summary-container">
+              <h3>{title}</h3>
+              <p>{description}</p>
+              <p>Price: {price}</p>
+              <p>Stock: {stock}</p>
+              <img
+                src={URL.createObjectURL(images[0])}
+                alt={`preview-${images[0].name}`}
+                className="preview-image"
+              />
+            </div>
+            <div className="action-buttons">
+              <button className="cancel-button" onClick={handleCancelClick}>
+                Cancel
+              </button>
+              <button
+                className="continue-button"
+                onClick={handleSubmitButtonClick}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        );
+      default:
+        return <div>Error 404</div>;
     }
   };
 
