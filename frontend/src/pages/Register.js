c86c01539a54e6d5ad05a1a6a327cd88430867d7
CherngYuen <arrengithub11111@gmail.com>
2025-01-16 18:02:18 +0800

    Isolate product clicks for resident vouchers to individual categories

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,9 +1,9 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { auth } from "../firebaseConfig"
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebaseConfig";
 
 const RegisterPage = () => {
   const navigate = useNavigate();
@@ -35,7 +35,11 @@ const RegisterPage = () => {
     }
 
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       toast.success("Registration successful!");
       navigate("/");
     } catch (error) {
@@ -48,58 +52,70 @@ const RegisterPage = () => {
       <img src="/MWHlogo.png" alt="MWH Logo" className="login-logo" />
       <ToastContainer />
       <form onSubmit={handleRegister} className="space-y-6">
-          <div>
-            <label htmlFor="email" className="block text-3xl font-medium text-gray-700">
-              Username
-            </label>
-            <input
-              type="text"
-              id="email"
-              value={formData.email}
-              onChange={handleInputChange}
-              placeholder="example@mail.com"
-              className="w-[400px] px-3 py-2 border rounded-md focus:ring focus:ring-indigo-500 focus:outline-none"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="block text-3xl font-medium text-gray-700">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={formData.password}
-              onChange={handleInputChange}
-              placeholder="Enter Password"
-              className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-indigo-500 focus:outline-none"
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="confirmPassword" className="block text-3xl font-medium text-gray-700">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              value={formData.confirmPassword}
-              onChange={handleInputChange}
-              placeholder="Confirm Password"
-              className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-indigo-500 focus:outline-none"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full px-4 py-2 text-white bg-[#d5432d] rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-3xl font-medium text-gray-700"
           >
-            Register
-          </button>
-        </form>
-        <p className="text-3xl text-center text-gray-600 mt-4">
-          Already have an account? <a href="/" className="text-[#d5432d] hover:underline">Login</a>
-        </p>
+            Username
+          </label>
+          <input
+            type="text"
+            id="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="example@mail.com"
+            className="w-[400px] px-3 py-2 border rounded-md focus:ring focus:ring-indigo-500 focus:outline-none"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="password"
+            className="block text-3xl font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            placeholder="Enter Password"
+            className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-indigo-500 focus:outline-none"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="confirmPassword"
+            className="block text-3xl font-medium text-gray-700"
+          >
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleInputChange}
+            placeholder="Confirm Password"
+            className="w-full px-3 py-2 border rounded-md focus:ring focus:ring-indigo-500 focus:outline-none"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full px-4 py-2 text-white bg-[#d5432d] rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Register
+        </button>
+      </form>
+      <p className="text-3xl text-center text-gray-600 mt-4">
+        Already have an account?{" "}
+        <a href="/" className="text-[#d5432d] hover:underline">
+          Login
+        </a>
+      </p>
     </div>
   );
 };
